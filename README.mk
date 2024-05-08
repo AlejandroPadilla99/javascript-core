61b9d62d5ac6d09446243b89ae949921ff8dc934
alejandro <alejandrop@bluetrailsoft.com>
2024-05-08 13:13:00 -0600

    init commit

---
+++ README.mk
@@ -0,0 +1,40 @@
+# Javascript core course
+
+## Types
+
+### Primitive Types
+in Javascript everything is an object = false
+
+1- undefined
+2- string
+3- number
+4- boolean
+5- symbol
+
+undefined vs undeclared vs uninitialized
+
+Special Values
+1- NaN 
+
+Fundamental Objects
+
+### Abstract Operations
+
+# To String 
+# ToNumber
+# ToBoolean
+
+
+### Coercion
+Coercion refers to the process of automatic or implicit conversion of values from one data type to another. This includes conversion from Numbers to String, etc.
+
+## Boxing
+
+
+### Equality
+### TypeScipt etc
+
+
+## Scope 
+
+## Object (Oriented)
